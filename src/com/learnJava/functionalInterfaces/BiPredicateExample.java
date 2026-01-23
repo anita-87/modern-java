1e0fb1f91ff88a0ac4edbccb6ac808118998ab8c
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2026-01-23 21:08:06 +0100

    lab: implementation Function inferface - part 1

--- src/com/learnJava/functionalInterfaces/BiPredicateExample.java
+++ src/com/learnJava/functionalInterfaces/BiPredicateExample.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
-import java.util.function.Predicate;
 
 public class BiPredicateExample {
     BiPredicate<Integer,Double> biPredicate = (gradeLevel, gpa) -> gradeLevel>=3 && gpa>=3.9;
---
+++ src/com/learnJava/functionalInterfaces/FunctionExample1.java
@@ -0,0 +1,13 @@
+package com.learnJava.functionalInterfaces;
+
+public class FunctionExample1 {
+
+    static String performConcat(String str) {
+        return FunctionExample.addSomeString.apply(str);
+    }
+
+    public static void main(String[] args) {
+        String result = performConcat("Hello");
+        System.out.println("Result: " + result);
+    }
+}
