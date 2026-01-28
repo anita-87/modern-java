526610b520b89893b6932e480f3e0907dbd4d406
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2026-01-28 20:50:50 +0100

    lab: Stream operations: distinct, count, sorted

--- src/com/learnJava/streams/StreamsFlatMapExample.java
+++ src/com/learnJava/streams/StreamsFlatMapExample.java
@@ -11,10 +11,21 @@ public class StreamsFlatMapExample {
         return StudentDataBase.getAllStudents().stream() //Stream<Student>
                 .map(Student::getActivities) //Stream<List<String>>
                 .flatMap(List::stream) // Stream<String>
+                .distinct() // Stream<String> -> removes duplicates
+                .sorted() // Sort the stream in alphabetical order
                 .toList(); // List<String>
+    }
+
+    public static long getStudentActivitiesCount() {
+        return StudentDataBase.getAllStudents().stream() //Stream<Student>
+                .map(Student::getActivities) //Stream<List<String>>
+                .flatMap(List::stream) // Stream<String>
+                .distinct() // Stream<String> -> removes duplicates
+                .count(); // number of elements of the Stream
     }
 
     public static void main(String[] args) {
         System.out.println(printStudentActivities());
+        System.out.println(getStudentActivitiesCount());
     }
 }
