c1f3b91ed1f31e8ab5990db699b9817d29f73dd1
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2026-01-30 20:39:37 +0100

    lab: Stream operation reduce part 2

--- src/com/learnJava/streams/StreamReduceExample.java
+++ src/com/learnJava/streams/StreamReduceExample.java
@@ -1,5 +1,8 @@
 package com.learnJava.streams;
 
+import com.learnJava.data.Student;
+import com.learnJava.data.StudentDataBase;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,6 +24,11 @@ public class StreamReduceExample {
                 .reduce((a, b) -> a*b);
     }
 
+    public static Optional<Student> getHighestGPAStudent() {
+        return StudentDataBase.getAllStudents().stream()
+                .reduce((s1, s2) -> s1.getGpa()>s2.getGpa() ? s1 : s2);
+    }
+
     public static void main(String[] args) {
         List<Integer> integers = Arrays.asList(1, 3, 5, 7);
         List<Integer> integers1 = new ArrayList<>();
@@ -35,5 +43,8 @@ public class StreamReduceExample {
         if (result1.isPresent()) {
             System.out.println(result1.get());
         }
+
+        Optional<Student> studentOptional = getHighestGPAStudent();
+        studentOptional.ifPresent(System.out::println);
     }
 }
