dea42f3270acd64280da574777421e2a39a5784f
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2026-02-06 19:24:35 +0100

    lab: Stream operation max using reduce

---
+++ src/com/learnJava/streams/StreamsMinMaxExample.java
@@ -0,0 +1,47 @@
+package com.learnJava.streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamsMinMaxExample {
+
+    public static int findMaxValue(List<Integer> integerList) {
+        return integerList.stream()
+                // 6  -> y
+                // 7  -> y
+                // 8  -> y
+                // 9  -> y
+                // 10 -> y
+                // x holds the max value for each element in the iteration
+                .reduce(0, (x, y) -> x>y ? x : y);
+    }
+
+    public static Optional<Integer> findMaxValueOptional(List<Integer> integerList) {
+        return integerList.stream()
+                // 6  -> y
+                // 7  -> y
+                // 8  -> y
+                // 9  -> y
+                // 10 -> y
+                // x holds the max value for each element in the iteration
+                .reduce((x, y) -> x>y ? x : y);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(6,7,8,9,10);
+
+        System.out.println("max value is: " + findMaxValue(integerList));
+
+        List<Integer> emptyList = new ArrayList<>();
+        System.out.println("max value is: " + emptyList);
+
+        Optional<Integer> maxValueOptional = findMaxValueOptional(emptyList);
+        if (maxValueOptional.isPresent()) {
+            System.out.println("max value using optional: " + maxValueOptional.get());
+        } else {
+            System.out.println("Input list is empty");
+        }
+    }
+}
